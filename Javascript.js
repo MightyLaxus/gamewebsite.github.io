cafaae1e0efce616f9afea840b4f9d56232aa744
MightyLaxus <ishumehta7461@gmail.com>
2022-11-04 23:09:14 +0530

    my game dev website

---
+++ Javascript.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    alert(document.getElementById("email").value + "\n" + 
+            document.getElementById("subject").value + "\n" + 
+            document.getElementById("textarea").value)
+}
