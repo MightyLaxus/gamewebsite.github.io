0214b50b0e73ee1079cd4763173efef127ef134e
MightyLaxus <ishumehta7461@gmail.com>
2022-11-16 13:28:46 +0530

    Code

--- Javascript.js
+++
@@ -1,5 +0,0 @@
-function myFunction() {
-    alert(document.getElementById("email").value + "\n" + 
-            document.getElementById("subject").value + "\n" + 
-            document.getElementById("textarea").value)
-}
